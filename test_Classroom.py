bd883c0dbe68f21a04f62324821180747b3f9845
Zenovii2017 <35252560+Zenovii2017@users.noreply.github.com>
2018-02-26 19:35:07 +0200

    Add files via upload

---
+++ test_Classroom.py
@@ -0,0 +1,34 @@
+def Test_Classroom():
+    errors = 0
+    try:
+        import Classroom
+    except:
+        errors += 1
+    try:
+        classroom_016 = Classroom.Classroom('016', 80, ['PC', 'projector',\
+                                                        'mic'])
+        classroom_007 = Classroom.Classroom('007', 12, ['TV'])
+    except:
+        errors += 1
+    try:
+        classroom_016.number
+        classroom_016.capacity
+        classroom_016.equimpent
+    except:
+        errors += 1
+    try:
+        classroom_016.equipment_differences(classroom_007)
+    except:
+        errors += 1
+    try:
+        print(classroom_016)
+    except:
+        errors += 1
+    try:
+        [classroom_016]
+    except:
+        errors += 1
+    return errors
+
+
+print(Test_Classroom())
